492200df85f1d58426e8fe9cf4ea8bdd5305a5b9
nickkeepfer <45045579+nickkeepfer@users.noreply.github.com>
2018-11-15 09:23:18 +0000

    Update dipolepotential.py
    This produces a "beam looking" potential but it's of order 10^(-20) , so I think some constants may be off

--- dipolepotential.py
+++ dipolepotential.py
@@ -1,61 +1,30 @@
 import numpy as np
-
 import matplotlib.pyplot as plt
-
-
 
 
 def testplot(wlen, power, w, w_0):
 
-
-
-
     ## Create Field ##
-
-    x = y = z = np.linspace(0,10,100)
-
-    #x, y, z = np.mgrid(X,Y,Z)
-    N=100
-
-    #u = np.zeros(N,N)
-
-    
+    x = y = np.arange(-30, 30, 0.1)
+    x, y = np.meshgrid(x, y)
 
     ## Constants ##
-
-    D_0 = 6*(3.33e-30)
-
+    D_0 = 6 * (3.33e-30)
     hbar = 1.05e-34
-
     c = 3e+8
-
     eps_0 = 8.85e-12
 
-    
-
-    ## Calculated Consants
-
+    ## Calculated Consants ##
     Delta = w - w_0
-
     rho = x**2 + y**2
 
-
-
-
     ## Main calc ##
-
-    u = (D_0**2)/(2*hbar*Delta)*(4*power)/(np.pi*w_0*c*eps_0)*((w_0/w)**2)*np.exp(-2*rho**2/w**2)
-
-    return x,y,u
-
+    u = (D_0**2)/(2*hbar*Delta)*(4*power)/(np.pi*w_0*c*eps_0)*((w_0/w)**2)*np.exp(-2*(rho**2)/(w**2))
+    print(u)
+    return x, y, u
 
 
-
-(x1,y1,u1) = testplot(940e-9, 1, 900e-9, 940e-9)
-
-
-
-
-plt.contour([x1,y1],u1)
-
+(x1, y1, u1) = testplot(940e5, 5000, 400, 390)
+plt.contourf(x1, y1, u1,levels=100)
+plt.colorbar()
 plt.show()
