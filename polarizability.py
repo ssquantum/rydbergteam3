7eeca85eb570a2b7196f2ea932ee14b6f8d9e998
ssquantum <45043843+ssquantum@users.noreply.github.com>
2018-11-21 10:50:30 +0000

    small update to polarizability
    added a line that shows the laser wavelength and changed what approximations are plotted

--- polarizability.py
+++ polarizability.py
@@ -11,7 +11,7 @@ import DipoleInt as DI
 from DipoleInt import c, eps0, hbar, a0, e
 
 # laser beam properties
-wavelength = np.logspace(-7, -5, 10000)   # wavelength of laser in m
+wavelength = np.logspace(-6.3, -5.5, 100000)   # wavelength of laser in m
 omega = 2*np.pi * c / wavelength          # angular frequency in rad/s
 power = 1                                 # power in watts
 beamwaist = 1e-6                          # beam waist in m
@@ -29,8 +29,10 @@ Csomega0 = 2*np.pi*c /Cswavelengths             # angular frequency in rad/s
 Csgamma = 2*np.pi*np.array([4.575, 5.234, 0.1263, 0.2922])*1e6      # natural linewidth in rad/s
 
 # create the dipole object and calculate the polarizability
-Cs_d = DI.dipole(133, 7/2., CsD0s, bprop, Csomega0, Csgamma)
-Csalpha = Cs_d.polarizability()            # polarizability without RWA
+# apart from the resonance at 456nm, including the 7p transitions is indistinguishable
+# instead we will compare the use of RWA with D1 and D2 transitions
+Cs_d = DI.dipole(133, 7/2., CsD0s[:2], bprop, Csomega0[:2], Csgamma[:2])
+Csalpha = Cs_d.RWApolarizability()            # polarizability with RWA
 Csalpha *= 1 / 4. / np.pi / eps0  /(a0)**3 # convert to units of Bohr radius cubed (cgs)
 
 # compare the effect of including the D1, D2 transition:
@@ -38,6 +40,7 @@ Cs_Dboth = DI.dipole(133, 7/2., CsD0s[:2], bprop, Csomega0[:2], Csgamma[:2])
 D1alpha = Cs_Dboth.polarizability()
 D1alpha *= 1 / 4. / np.pi / eps0  /(a0)**3 # convert to units of Bohr radius cubed (cgs)
 
+# only the D2 transition
 Cs_D2 = DI.dipole(133, 7/2., CsD0s[1], bprop, Csomega0[1], Csgamma[1])
 D2alpha = Cs_D2.polarizability()
 D2alpha *= 1 / 4. / np.pi / eps0  /(a0)**3 # convert to units of Bohr radius cubed (cgs)
@@ -46,12 +49,14 @@ plt.figure()
 plt.title("Polarizability for ground state $^{133}$Cs")
 # Cs_d and Cs_Dboth are indistinguishable except for the resonances at 456nm
 # so four our laser working at 980nm, we can just use the D1 and D2 transitions.
-plt.semilogx(wavelength, Csalpha, label="Strongest 4 transitions")
+plt.semilogx(wavelength, Csalpha, label="D1 and D2 transitions with RWA")
 plt.semilogx(wavelength, D1alpha, label="D1 and D2 transitions")
 plt.semilogx(wavelength, D2alpha, label="Just D2 transition")
+ymax = 2e3
+plt.plot([980e-9]*2, [-ymax, ymax], 'k--', label="Laser at 980nm")
 plt.xlabel("Wavelength (m)")
 plt.ylabel("Polarizability (a$_0$$^3$)")
-plt.ylim((-1e3,1e3))
+plt.ylim((-ymax,ymax))
 plt.tight_layout()
 plt.legend()
 plt.show()
